2a9f9f93df47d818d1c390bb9e37f4fe02cd11c0
Thigo <thiago.l17@yahoo.com>
2021-01-16 02:56:00 -0300

    app

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ function tweet(msg) {
     consumer_key: process.env.CONSUMER_KEY,
     consumer_secret: process.env.CONSUMER_SECRET,
     access_token: process.env.ACCESS_TOKEN,
-    access_token_secret: process.env.ACCESS_TOKEN_SCRET,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
     timeout_ms: 60 * 1000,
     strictSSL: true
   });
